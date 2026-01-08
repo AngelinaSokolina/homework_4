52d878f1a7c7733bbb5df614c2379916930672ad
Your Name <akusilda@proton.me>
2026-01-08 19:03:02 +0300

    Error correction

--- src/main.py
+++ src/main.py
@@ -1,29 +1,22 @@
-# Сложение чисел
-def add_numbers(a, b):
+def add_numbers(a: int | float, b: int | float) -> float:
+    """Сложение чисел"""
     return a + b
 
 
-# Четное ли число?
-def is_even(a):
+def is_even(a: int | float) -> bool:
+    """Четное ли число?"""
     return a % 2 == 0
 
-# Найти максимальное значение
+
 def find_max(my_list):
+    """Найти максимальное значение"""
     if len(my_list) > 0:
         return max(my_list)
     return 0
 
 
-def divide(a, b):
+def divide(a: int | float, b: int | float) -> float:
+    """Деление чисел"""
     if b > 0:
         return a / b
     return 0
-
-if __name__ == '__main__':
-    assert add_numbers(2, 2) == 4
-
-    assert is_even(2) == True
-    assert is_even(3) == False, 'Это число нечетное'
-
-    assert find_max([1, 2, 3, 4, 5]) == 5
-    assert find_max([]) == 0
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,27 @@
-from main import divide
+from src.main import divide
+from src.main import add_numbers
+from src.main import is_even
+from src.main import find_max
 
 
-def test_main():
-    assert divide(2,1) == 2
+def test_divide() -> None:
+    """Тестирование функции divide"""
+    assert divide(2, 1) == 2
+    assert divide(2, 0) == 0
 
-    assert divide(2,0) == 0
+
+def test_add_numbers() -> None:
+    """Тестирование функции add_numbers"""
+    assert add_numbers(2, 2) == 4
+
+
+def test_is_even() -> None:
+    """Тестирование функции is_even"""
+    assert is_even(2) == True
+    assert is_even(3) == False, 'Это число нечетное'
+
+
+def test_find_max() -> None:
+    """Тестирование функции find_max"""
+    assert find_max([1, 2, 3, 4, 5]) == 5
+    assert find_max([]) == 0
